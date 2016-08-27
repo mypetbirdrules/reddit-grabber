8e58c145a544967d813a35c7ac16986436952c9a
mypetbirdrules <marshallmason5@gmail.com>
2016-08-27 00:54:50 -0400

    Finished basic Imgur extraction section

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+# Creating argument parser
+parserObj = argparse.ArgumentParser(description="A simple Python Reddit scraper")
+
+# Configuring argument parser
+parser.add_argument("--subreddit")
+
+if __name__ == "__main__":
+  
