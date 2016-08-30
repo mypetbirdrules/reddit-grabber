e14105df66d2c18cc9f0958729154fbc63000637
mypetbirdrules <marshallmason5@gmail.com>
2016-08-30 06:21:19 -0400

    Fixed Python3 indentation issues

--- imgur.py
+++ imgur.py
@@ -4,30 +4,25 @@ import re
 import requests
 
 def extractImageURL(imgurURL):
-  if re.match(".*\.imgur.com\/[a-zA-Z]+") != false:
-    # URL points to a single Imgur image
-    
-    # make a get request at imgur.com/xxxx.jpg
-    # read Content-Type from http headers to determine file type
+    if re.match(".*imgur\.com\/[a-zA-Z]+", imgurURL) != False:
+        # make a get request at imgur.com/xxxx.jpg
+        # read Content-Type from http headers to determine file type
+        getRequest = requests.get(imgurURL + ".jpg")
 
-    getRequest = requests.get(imgurURL + ".jpg")
+        if getRequest.status_code == 200:
+            # webpage exists
+            # assigning file extension to URL
+            responseHeader = getRequest.headers["Content-Type"]
 
-    if getRequest.status_code == 200:
-      # webpage exists
-      # assigning file extension to URL
-      responseHeader = getRequest.headers["Content-Type"]
-
-      if responseHeader == "image/jpeg":
-	return (imgurURL + ".jpg")
-      elif responseHeader == "image/png":
-	return (imgurURL + ".png")
-      elif responseHeader == "image/gif":
-	return (imgurURL + ".gif")
-
-  else:
-    
-    # return False for following reasons
-    # horrible errors
-    # unsupported features (Imgur albums/galleries)
-
-    return False
+            if responseHeader == "image/jpeg":
+                return imgurURL + ".jpg"
+            elif responseHeader == "image/png":
+                return imgurURL + ".png"
+            elif responseHeader == "image/gif":
+                return imgurURL + ".gif"
+	
+        else:
+            # return False for following reasons
+            # horrible errors
+            # unsupported features (Imgur albums/galleries)
+            return False
