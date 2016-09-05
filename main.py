52556a8690567c88aefd0db5bce91170cc7620d5
mypetbirdrules <marshallmason5@gmail.com>
2016-09-05 17:51:01 -0400

    Added output verbosity

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import argparse
 import sys
+import os
 import imgur
 import praw
 import re
@@ -19,8 +20,8 @@ if __name__ == "__main__":
     parserObj.add_argument("-o", action="store", default="redditurls.txt", dest="outputFilename", type=str, help="output file for scraped URLs")
     parserObj.add_argument("--filter", action="store", default="hot", dest="filter", type=str, help="subreddit content sort method (default=hot)")
     parserObj.add_argument("-n", action="store", default=100, dest="limit", type=int, help="number of posts to scrape")
-    parserObj.add_argument("--regex", action="store", default=".*", dest="titleRegex", type=str, help="Filter posts with a regular expression (default=.*)")
-    parserObj.add_argument("--links-only", action="store_true", default=False, dest="removeTextPosts", help="do not scrape text posts (default=False)")
+    parserObj.add_argument("--title-regex", action="store", default=".*", dest="titleRegex", type=str, help="filter posts by title with a regexp (default=.*)")
+    parserObj.add_argument("--link-regex", action="store", default=".*", dest="linkRegex", type=str, help="filter posts by URL with a regexp (default=.*)")
     parserObj.add_argument("-v", action="store_true", default=False, dest="verboseFlag", help="toggle verbose script output")
 
     arguments = parserObj.parse_args(sys.argv[1:])
@@ -41,21 +42,35 @@ if __name__ == "__main__":
         elif arguments.filter == "rising":
             submissions = subredditObject.get_rising(limit=arguments.limit)
         else:
-            print("Invalid subreddit sort filter")
+            print("Runtime Error: Invalid subreddit sort filter")
             sys.exit(1)
         
+        # count number of grabbed get_content objects
+        grabCount = 0
 
         for post in submissions:
-            
             # if the post is not a link, PRAW will return the link to the text post
             if re.match(arguments.titleRegex, post.title):
+                # increment grabCount for every correct regex title match
+                grabCount += 1
+
+                if arguments.verboseFlag == True:
+                    print(str(grabCount)+": "+post.title+" - match!")
+
                 if arguments.imgurResolve == True:
                     outputFile.write(imgur.extractImageURL(post.url)+"\n")
                 else:
                     outputFile.write(post.url+"\n")
 
-        print("Success")
-        outputFile.close()
+        if grabCount > 0:
+            print("Success - reddit-grabber scraped "+str(grabCount)+(" post"," posts")[grabCount>1])
+            outputFile.close()
+        else:
+            print("reddit-grabber grabbed 0 posts - deleting output file...")
+            outputFile.close()
+            os.remove(arguments.outputFilename)
+            print(arguments.outputFilename+" deleted")
+            
     else:
-        print("Limit must be over 0")
+        print("Runtime Error: Scrape limit must be over 0")
         sys.exit(1)
